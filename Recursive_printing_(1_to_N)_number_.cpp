bd35d55c54bdbe564f5e632c55ad6016c03d9801
Ajay Singh Rana <ajayrana68359@gmail.com>
2023-01-24 23:30:26 +0530

    recursion

---
+++ Recursive_printing_(1_to_N)_number_.cpp
@@ -0,0 +1,19 @@
+// print 1 to N NUMBERS BY RECURSION
+#include <iostream>
+using namespace std;
+void print(int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    print(num - 1);
+    cout << num << " ";
+}
+int main()
+{
+    int num;
+    cin >> num;
+    print(num);
+    return 0;
+}
